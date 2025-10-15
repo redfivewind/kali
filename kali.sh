44c3f7098c2ad6e870c512f7d8aafdff0387274b
redfivewind <36482775+redfivewind@users.noreply.github.com>
2025-10-15 11:00:46 +0000

    Update kali.sh

--- kali.sh
+++ kali.sh
@@ -8,20 +8,27 @@ sudo apt update -y
 sudo apt upgrade -y
 sudo apt autoremove -y
 
-# Install Docker
-echo "[*] Installing Docker..."
+# Setup Docker
+echo "[*] Setting up Docker..."
 sudo apt install -y docker.io
 sudo apt install -y docker-compose
 sudo usermod -aG docker $USER
+sudo systemctl enable docker --now
 
-# Install Bloodhound
-echo "[*] Installing Bloodhound..."
-#FIXME
+# Setup Bloodhound
+echo "[*] Setting up Bloodhound..."
+cd /tmp
+wget https://github.com/SpecterOps/bloodhound-cli/releases/latest/download/bloodhound-cli-linux-amd64.tar.gz
+tar -xvzf bloodhound-cli-linux-amd64.tar.gz
+chmod +x ./bloodhound-cli
+sudo ./bloodhound-cli install
+cd
 
-# Install seclists
-echo "[*] Installing seclists..."
-sudo apt install -y seclists
-
-# Configure services
-echo "[*] Configuring services..."
+# Setup Metasploit
+echo "[*] Setting up Metasploit..."
 sudo systemctl enable postgresql --now
+sudo msfdb init
+
+# Setup seclists
+echo "[*] Setting up seclists..."
+sudo apt install -y seclists
