dae84541c006774df1aa06969d8681f17a08fe8e
redfivewind <36482775+redfivewind@users.noreply.github.com>
2026-04-17 06:59:48 +0000

    Update kali.sh

--- kali.sh
+++ kali.sh
@@ -115,7 +115,7 @@ echo "export PATH=\$PATH:\$GOROOT/bin:\$GOPATH/bin" >> ~/.zshrc
 # Configure GRUB2
 echo "[*] Configuring GRUB2 (if applicable)..."
 if [ -f /etc/default/grub ]; then
-    sed -i "s/^\GRUB_CMDLINE_LINUX=\"/&rd.driver.blacklist=nouveau modprobe.blacklist=nouveau /" /etc/default/grub
+    sed -i "s/^\GRUB_CMDLINE_LINUX_DEFAULT=\"/&rd.driver.blacklist=nouveau modprobe.blacklist=nouveau /" /etc/default/grub
 fi
 
 # Configure Linux kernel
